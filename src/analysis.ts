57d72d46c1dc2f808b618214aed52a0cb4287739
Jonas Koneczny <112870800+JonasKoneczny@users.noreply.github.com>
2025-04-05 12:27:03 +0200

    added csv download as fallback option

---
+++ src/analysis.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs";
+import * as path from "path";
+import { PlayerEntry } from './matchday-fetcher';
+
+const gradesToPoints = {
+    "100": 10,
+    "150": 8,
+    "200": 6,
+    "250": 4,
+    "300": 2,
+    "350": 0,
+    "400": -2,
+    "450": -4,
+    "500": -6,
+    "550": -8,
+    "600": -10
+}
+
+function loadAllMatchdays(): PlayerEntry[][] {
+    const dir = "./data";
+    const files = fs.readdirSync(dir).filter(f => f.endsWith(".json"));
+    return files.map(file => {
+        const raw = fs.readFileSync(path.join(dir, file), "utf-8");
+        return JSON.parse(raw) as PlayerEntry[];
+    });
+}
+
+function getTopPlayersFirstHalf(matchdays: PlayerEntry[][], topN = 100) {
+    return getTopPlayersOfMatchdays(matchdays, topN, 0, 17)
+}
+
+function getTopPlayersSecondHalf(matchdays: PlayerEntry[][], topN = 100) {
+    return getTopPlayersOfMatchdays(matchdays, topN, 18, 34)
+}
+
+function getTopPlayersOfMatchdays(matchdays: PlayerEntry[][], topN = 100, start: number, end: number) {
+    const playerTotals: Record<string, { name: string; points: number }> = {};
+
+    matchdays.slice(start, end).forEach(day =>
+        day.forEach((player: PlayerEntry) => {
+            if(!playerTotals[player.playerId]) {
+                playerTotals[player.playerId] = {name: player.name, points: 0};
+            }
+            playerTotals[player.playerId].points += player.points ?? 0;
+        })
+    );
+
+    console.log(playerTotals)
+
+    return Object.entries(playerTotals)
+        .sort(([, a], [, b]) => b.points - a.points)
+        .slice(0, topN);
+}
+
+const matchdays = loadAllMatchdays();
+console.log(matchdays)
+//console.log("Top Hinrunde Spieler:", getTopPlayersFirstHalf(matchdays));
--- src/matchday-fetcher.ts
+++ src/matchday-fetcher.ts
@@ -2,54 +2,48 @@ import axios from "axios";
 import * as fs from "fs";
 import * as path from "path";
 import { parse } from 'csv-parse';
+import { promises as fsPromises } from "fs";
+
 
 const PLAYER_CSV_URL = "https://www.kicker-libero.de/api/sportsdata/v1/players-details/se-k00012024.csv";
 const MATCHDAY_BASE_URL = "https://www.kicker-libero.de/api/gameloop/v1/state/round/";
 const SEASON_PREFIX = "rn-k0001202400";
 const MAX_MATCHDAY = 34;
+const LOCAL_MATCHDAY_PATH = './data/matchdays'
+const LOCAL_PLAYERS_PATH = './data/players/players-se-k00012024.csv'
 
-async function fetchCSVPlayerData(): Promise<Record<string, string>> {
-    const response = await axios.get(PLAYER_CSV_URL);
-
-    const records: any[] = await new Promise<any[]>((resolve, reject) => {
-        parse(response.data, {
-            delimiter: ";",
-            columns: true,
-            skip_empty_lines: true,
-        }, (err, output) => {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(output);
-            }
-        });
-    });
-
-    const playerMap: Record<string, string> = {};
-    for(const record of records) {
-        const id = record["ID"];
-        const name = record["Angezeigter Name"];
-        if(id && name) {
-            playerMap[id] = name;
-        }
-    }
-    return playerMap;
+export interface OriginalPlayerEntry {
+    id: string
+    points: number
+    status: string
+    pointsBreakDown: {
+        grade: number | null
+        goals: number | null
+        assists: number | null
+        pointsGrade: number | null
+        pointsGoals: number | null
+        pointsCards: number | null
+        pointsAssists: number | null
+        pointsStarter: number | null
+        pointsMvp: number | null
+        pointsJoker: number | null
+        pointsCleanSheet: number | null
+    },
+    missedInfo: string | null
 }
 
-async function fetchMatchdayData(id: string): Promise<any | null> {
-    const url = MATCHDAY_BASE_URL + id + ".json";
-    try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch(err: any) {
-        if(err.response?.status === 404) {
-            return null;
-        }
-        throw err;
-    }
-}
+export type PlayerEntryKey = "ID" |
+    "Vorname" |
+    "Nachname" |
+    "Angezeigter Name (kurz)" |
+    "Angezeigter Name" |
+    "Verein" |
+    "Position" |
+    "Marktwert" |
+    "Punkte" |
+    "Notendurchschnitt"
 
-interface PlayerEntry {
+export interface PlayerEntry {
     playerId: string;
     name: string;
     points: number;
@@ -70,12 +64,96 @@ interface PlayerEntry {
     };
 }
 
-async function mergeAndSaveMatchday(playerMap: Record<string, string>, matchdayId: string): Promise<void> {
+async function fetchCSVPlayerData(): Promise<Record<string, Record<"ID" | "Vorname" | "Nachname" | "Angezeigter Name (kurz)" | "Angezeigter Name" | "Verein" | "Position" | "Marktwert" | "Punkte" | "Notendurchschnitt", string>>> {
+    const response = await axios.get(PLAYER_CSV_URL);
+
+    const records: any[] = await new Promise<any[]>((resolve, reject) => {
+        parse(response.data, {
+            delimiter: ";",
+            columns: true,
+            skip_empty_lines: true,
+        }, (err, output) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(output);
+            }
+        });
+    });
+
+    const playerMap: Record<string, Record<PlayerEntryKey, string>> = {};
+
+    for(const record of records) {
+        const id = record["ID"];
+        const name = record["Angezeigter Name"];
+        if(id && name) {
+            playerMap[id] = name;
+        }
+    }
+    fs.mkdirSync("./data/players", {recursive: true});
+    fs.writeFileSync("./data/players/players-se.json", JSON.stringify(records, null, 2), "utf-8");
+    return playerMap as Record<string, Record<PlayerEntryKey, string>>;
+}
+
+async function fetchMatchdayData(id: string): Promise<any | null> {
+    const url = MATCHDAY_BASE_URL + id + ".json";
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch(err: any) {
+        if(err.response?.status === 404) {
+            return null;
+        }
+        throw err;
+    }
+}
+
+async function getPlayersData() {
+    try {
+        const data = await fsPromises.readFile(LOCAL_PLAYERS_PATH, "utf-8");
+
+        const records: any[] = await new Promise<any[]>((resolve, reject) => {
+            parse(data, {
+                delimiter: ";",
+                columns: true,
+                skip_empty_lines: true,
+            }, (err, output) => {
+                if (err) reject(err);
+                else resolve(output);
+            });
+        });
+
+        const playerMap: Record<string, Record<PlayerEntryKey, string>> = {};
+        for (const record of records) {
+            const id = record["ID"];
+            const name = record["Angezeigter Name"];
+            if (id && name) {
+                playerMap[id] = record;
+            }
+        }
+
+        return playerMap;
+    } catch (err) {
+        console.info("ℹ️ no local CSV found, load remote");
+        return await fetchCSVPlayerData();
+    }
+}
+
+async function saveMatchday(matchdayId: string) {
     const matchdayData = await fetchMatchdayData(matchdayId);
+    const matchDayFilename = 'original-' + matchdayId + '.json'
+    const matchDayOutputPath = path.join(LOCAL_MATCHDAY_PATH, matchDayFilename);
+    fs.mkdirSync(LOCAL_MATCHDAY_PATH, {recursive: true});
+    fs.writeFileSync(matchDayOutputPath, JSON.stringify(matchdayData, null, 2), "utf-8");
     if(!matchdayData) {
         console.log("⛔️ No data for " + matchdayId);
         return;
     }
+
+    console.log("✅ Saved " + matchDayFilename);
+}
+
+async function mergeAndSaveMatchday(playerMap: Record<string, Record<"ID" | "Vorname" | "Nachname" | "Angezeigter Name (kurz)" | "Angezeigter Name" | "Verein" | "Position" | "Marktwert" | "Punkte" | "Notendurchschnitt", string>>, matchdayId: string, matchdayData: any): Promise<void> {
 
     const playerStates = matchdayData.matches.flatMap((match: any) => match.players || []);
 
@@ -83,28 +161,29 @@ async function mergeAndSaveMatchday(playerMap: Record<string, string>, matchdayI
         return {
             playerId: entry.id,
             name: playerMap[entry.id] || entry.name?.display || "Unknown",
-            points: entry.score,
-            kickerGrade: entry.kickerGrade,
+            points: entry.points,
             teamId: entry.teamId,
             pointsBreakDown: entry.pointsBreakDown,
         };
     });
 
     const fileName = matchdayId + ".json";
-    const outputPath = path.join("./data", fileName);
-    fs.mkdirSync("./data", {recursive: true});
+    const outputPath = path.join("./data/combined", fileName);
+    fs.mkdirSync("./data/combined", {recursive: true});
     fs.writeFileSync(outputPath, JSON.stringify(result, null, 2), "utf-8");
     console.log("✅ Saved " + fileName);
 }
 
 async function runAllMatchdays(): Promise<void> {
-    const playerMap = await fetchCSVPlayerData();
-
+    const playerMap = await getPlayersData()
+    console.log(playerMap)
     for(let i = 1; i <= MAX_MATCHDAY; i++) {
         const matchdayId = SEASON_PREFIX + i.toString().padStart(2, "0");
-        await mergeAndSaveMatchday(playerMap, matchdayId);
+        const matchDayData = await saveMatchday(matchdayId)
+        await mergeAndSaveMatchday(playerMap, matchdayId, matchDayData);
         await new Promise((r) => setTimeout(r, 500));
     }
 }
 
-runAllMatchdays().catch(console.error);
+// runAllMatchdays().catch(console.error);
+console.log( getPlayersData().catch(e => console.error(e)).then(a => console.log(a)))
